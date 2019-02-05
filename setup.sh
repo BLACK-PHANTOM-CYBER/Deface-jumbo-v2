f163231040f3dcf203a0902737cb3d967426825f
BLACK PHANTOM CYBER <46845458+BLACK-PHANTOM-CYBER@users.noreply.github.com>
2019-02-05 13:20:22 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,36 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+echo
+echo
+toilet -f slant "JUMBO V.2" | lolcat
+echo
+echo
+echo 
+echo "=================================" |lolcat
+echo "CREATED:MR.SPOON" | lolcat
+echo "TEAM:BLACK PHANTOM CYBER" | lolcat
+echo "VERSION:V.2" | lolcat
+echo "JUMLAH WEB YG BISA DI TAMPUNG:100" | lolcat
+echo "=================================" |lolcat
+echo
+echo
+echo
+echo "MASUKAN NAMA ANDA===>"
+read nama
+echo
+echo
+clear
+sleep 1
+if [ $nama ]; then
+ echo  "welcome my tool $nama" | lolcat
+
+fi
+sleep 4
+echo
+sh start.sh
